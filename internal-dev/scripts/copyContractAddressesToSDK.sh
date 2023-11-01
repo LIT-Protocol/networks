e20f0fa916150c014397eb9558c094090081d6ed
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2023-11-01 14:24:54 -0400

    update addresses

--- internal-dev/scripts/copyContractAddressesToSDK.sh
+++ internal-dev/scripts/copyContractAddressesToSDK.sh
@@ -17,11 +17,14 @@ for key in "${keys_from_json[@]}"; do
   contract_name_in_sdk_without_sol=${contract_name_in_sdk%.sol}
   abi_path_js="$sdk_path/$contract_name_in_sdk/${contract_name_in_sdk_without_sol}Data.js"
   abi_path_ts="$sdk_path/$contract_name_in_sdk/${contract_name_in_sdk_without_sol}Data.ts"
+  abi_path_mjs="$sdk_path/$contract_name_in_sdk/${contract_name_in_sdk_without_sol}Data.mjs"
+
   echo "Replacing address for $contract_name_in_sdk in $abi_path_js"
 
   # Replace the address in the abi file
   sed -i '' -e 's/"address":.*/"address": "'"$value"'",/g' $abi_path_js
   sed -i '' -e 's/"address":.*/"address": "'"$value"'",/g' $abi_path_ts
+  sed -i '' -e 's/"address":.*/"address": "'"$value"'",/g' $abi_path_mjs
 
   ((counter++))
 done
